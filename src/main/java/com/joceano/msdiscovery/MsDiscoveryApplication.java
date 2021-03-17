b770cbd137c9572f37553d92c0698d22dc8e72ed
joceano <alves.joceano@gmail.com>
2021-03-16 22:16:50 -0300

    init

--- src/main/java/com/joceano/msdiscovery/MsDiscoveryApplication.java
+++ src/main/java/com/joceano/msdiscovery/MsDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.joceano.msdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsDiscoveryApplication {
 
 	public static void main(String[] args) {
